d637681259ef75795a6a7a8f3f309d3ed36381fc
FelipeAfana <fafanador@unab.edu.co>
2025-12-04 21:29:03 -0500

    Primeros pasos

---
+++ Juego 2D/Assets/Script/Player/Movimiento.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public float speed=5;
+    
+    private void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
